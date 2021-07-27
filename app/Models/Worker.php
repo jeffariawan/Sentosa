039d53ef410a207680c98512ea9ce75f49a15dc2
FawwaziAlamsyah <fwzalamsyah27@gmail.com>
2021-07-27 14:13:20 +0700

    menghubungkan tabel worker dan worker_level-model

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\WorkerLevel;
 
 class Worker extends Model
 {
     use HasFactory;
+    protected $table = 'worker';
+    protected $primaryKey = 'worker_id';
+    protected $guarded = [];
+
+    public function WorkerLevel()
+    {
+        return $this->belongsTo(WorkerLevel::class, 'worker_level_id', 'worker_level_id');
+    }
 }
--- app/Models/WorkerLevel.php
+++ app/Models/WorkerLevel.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class WorkerLevel extends Model
 {
     use HasFactory;
+    protected $table = 'worker_level';
+    protected $primaryKey = 'worker_level_id';
+    protected $guarded = [];
+    
+    public function Worker()
+    {
+        return $this->hasMany(Worker::class, 'worker_id', 'worker_id');
+    }
+
 }
