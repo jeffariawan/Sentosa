3227c4dcca3fd8366be90e6b90b204ea6a37e10f
FawwaziAlamsyah <fwzalamsyah27@gmail.com>
2021-08-01 16:16:13 +0700

    update tamplate baru

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,42 +1,24 @@
-  <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-        <span>FlexStart</span>
-      </a>
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="{{ route('home') }}">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a href="{{ route('article.index') }}">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
     </div>
-  </header><!-- End Header -->
+  </div>
+</nav>
--- resources/views/project/PostProjectStart.blade.php
+++ resources/views/project/PostProjectStart.blade.php
@@ -3,71 +3,51 @@
 @section('container')
 <!-- ======= Contact Section ======= -->
 <section id="contact" class="contact">
-    <div class="container" data-aos="fade-up">
-
+    <div class="container">
         <div class="row gy-4">
 
-            <div class="col-lg-3">
+            <div class="col-lg-2">
             </div>
 
-            <div class="col-lg-6">
-                <header class="section-header">
-                    <h2>Tell us what you need done</h2>
-                    <h4>skilled freelancers within minutes. View profiles, ratings, portfolios and chat with them. Pay the freelancer only when you are 100% satisfied with their work.</h4>
-                </header>
+            <div class="col-lg-8 bg-light">
 
                 <form action="{{ route('project.PostProjectStart.store') }}" method="post" class="php-email-form">
                     @csrf
                     <div class="row gy-4">
 
                         <div class="col-md-12">
-                            <h3>Choose a name for your project</h3>
-                            <input type="text" class="form-control" name="title" placeholder="Subject" required>
+                            <label>Choose a name for your project</label>
+                            <input name="title" class="form-control" type="text" placeholder="Default input" required>
                         </div>
 
-                        <div class="col-md-12">
-                            <h3>Tell us more about your project</h3>
-                            <p>Start with a bit about yourself or your business, and include an overview of what you need done.
-                            </p>
-                            <textarea class="form-control" name="description" rows="4" placeholder="Describe your project here..."
-                                required></textarea>
-                                <select class="selectpicker" multiple data-live-search="true">
-                                    <option>Mustard</option>
-                                    <option>Ketchup</option>
-                                    <option>Barbecue</option>
-                                  </select>
-                                  
-                        </div>
+                        <div class="form-group col-md-12">
+                            <label for="exampleFormControlTextarea1">Tell us more about your project</label>
+                            <textarea name="description" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Describe your project here..."  required></textarea>
+                          </div>
 
-                        <div class="col-md-12">
+                          <div class="col-md-12">
                             <h4>What skills are required?</h4>
                             <p>Enter up to 5 skills that best describe your project. Freelancers will use these skills to find projects they are most interested and experienced in.</p>                        
                                 @foreach($service as $srvc )
                                 <div class="form-check">
                                     <label class="form-check-label" for="flexCheckDefault">
                                     <input class="form-check-input" type="checkbox" name="refServiceId[]" value="{{ $srvc->ref_service_id }}" >
-                                   
                                         {{ $srvc->name }}
                                     </label>
                                 </div>
                                 @endforeach
                         </div>
-
                         
-
                         <div class="col-md-12">
-                            <div class="loading">Loading</div>
-                            <div class="error-message"></div>
-                            <div class="sent-message">Your message has been sent. Thank you!</div>
                             <button type="submit">Next</button>
                         </div>
 
                     </div>
                 </form>
 
-            </div>
+            </div> 
 
-            <div class="col-lg-3">
+            <div class="col-lg-2">
             </div>
 
         </div>
